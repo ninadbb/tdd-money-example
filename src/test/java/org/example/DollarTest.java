db042d99fb62da26f78484f86e7400bde28d8fc2
Ninad <ninadbb@gmail.com>
2020-10-27 23:14:51 +0530

    added testMultiplication

---
+++ src/test/java/org/example/DollarTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DollarTest {
+
+    @Test
+    public void testMultiplication(){
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
+        assertEquals(10, five.amount);
+        product = five.times(3);
+        assertEquals(15, five.amount);
+
+    }
+}
