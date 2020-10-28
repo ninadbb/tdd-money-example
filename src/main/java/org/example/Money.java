cec209ce298ce422f514884bd1e1399d8e0f4762
Ninad <ninadbb@gmail.com>
2020-10-28 22:43:47 +0530

    Makin' Objects => Dollar/Franc duplication

--- src/main/java/org/example/Money.java
+++ src/main/java/org/example/Money.java
@@ -1,11 +1,21 @@
 package org.example;
 
-public class Money {
+public abstract class Money {
     protected int amount;
 
     public Money(int amount) {
         this.amount = amount;
     }
+
+    public static Money franc(int amount) {
+        return new Franc(amount);
+    }
+
+    public static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    abstract Money times(int multiplier);
 
     @Override
     public boolean equals(Object o) {
