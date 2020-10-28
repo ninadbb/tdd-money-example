13489773392eebf14b4e631e84ae140069f69274
Ninad <ninadbb@gmail.com>
2020-10-28 22:28:19 +0530

    Equality for All, Redux

--- src/main/java/org/example/Dollar.java
+++ src/main/java/org/example/Dollar.java
@@ -1,20 +1,13 @@
 package org.example;
 
-public class Dollar {
-    private int amount;
+public class Dollar extends Money {
 
     public Dollar(int amount) {
-        this.amount = amount;
+        super(amount);
     }
 
     public Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        Dollar dollar = (Dollar) o;
-        return amount == dollar.amount;
     }
 
 }
---
+++ src/main/java/org/example/Money.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Money {
+    protected int amount;
+
+    public Money(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Money money = (Money) o;
+        return amount == money.amount;
+    }
+}
