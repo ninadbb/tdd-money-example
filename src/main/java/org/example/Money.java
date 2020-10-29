aa057ee737f4ddebce27bfde656c9b5705efe249
Ninad <ninadbb@gmail.com>
2020-10-29 21:35:36 +0530

    Addition finally

--- src/main/java/org/example/Money.java
+++ src/main/java/org/example/Money.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class Money {
+public class Money implements Expression{
     protected int amount;
     protected String currency;
 
@@ -34,5 +34,9 @@ public class Money {
 
     public String toString() {
         return amount + " " + currency;
+    }
+
+    public Expression plus(Money addend) {
+        return new Money(amount + addend.amount, currency);
     }
 }
