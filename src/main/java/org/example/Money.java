6119fdad9bc10e67dfab50c75ec10bb808a6dd92
Ninad <ninadbb@gmail.com>
2020-10-29 22:42:37 +0530

    Mixed currencies

--- src/main/java/org/example/Money.java
+++ src/main/java/org/example/Money.java
@@ -17,7 +17,7 @@ public class Money implements Expression {
         return new Money(amount, "USD");
     }
 
-    public Money times(int multiplier) {
+    public Expression times(int multiplier) {
         return new Money(amount * multiplier, currency);
     }
 
@@ -36,7 +36,7 @@ public class Money implements Expression {
         return amount + " " + currency;
     }
 
-    public Expression plus(Money addend) {
+    public Expression plus(Expression addend) {
         return new Sum(this, addend);
     }
 
